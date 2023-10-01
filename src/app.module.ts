c666ff059d9697fcedf8e7d2beed48c85bbb4ad0
Kamyar Ghazanfari <ghazanfarik79@gmail.com>
2023-10-02 03:06:13 +0330

    last commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,38 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
 import { GatewayModule } from './gateway/gateway.module';
+import { TokenModule } from './token/jwt.module';
+import { UserModule } from './user/user.module';
+import { MessageModule } from './messege/messege.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
-  imports: [GatewayModule],
+  imports: [
+    GatewayModule,
+    TokenModule,
+    UserModule,
+    MessageModule,
+    HttpModule,
+    EventEmitterModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    MongooseModule.forRoot(process.env.MONGO_DB_URI, {
+      dbName: process.env.MONGO_DB_DATABASE,
+      connectionFactory: (connection) => {
+        if (connection.readyState) {
+          console.log('Mongo database connected successfully.');
+        }
+
+        return connection;
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
+  global.__basedir = __dirname;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
+  app.use(
+    ['/api', '/api-json'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('VistaTest')
+    .setDescription('The VistaTest API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
